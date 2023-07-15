68854a2d88ab99668e428769744e99a852475978
Justin McCauliffe <jmccauli@gmail.com>
2023-07-15 14:16:23 -0400

    Switch out wallet data icons for local images instead of base64 encoded images

--- docker/web/components/ProviderIcon.tsx
+++ docker/web/components/ProviderIcon.tsx
@@ -1,12 +1,24 @@
 import Image from 'next/image'
 import { getProviderById } from '@/utils/wallets';
 import { useWallet } from '@txnlab/use-wallet'
+import deflyIcon from '@/images/defly.svg'
+import peraIcon from '@/images/pera.svg'
 
 type ProviderIconType = { 
   providerId: string;
   width?: number;
   height?: number;
 }
+
+type ProviderIconImage = {
+  [providerId: string]: string;
+}
+
+const providerIconImages: ProviderIconImage = {
+  'defly': deflyIcon,
+  'pera': peraIcon,
+}
+
 
 export default function ProviderIcon({ providerId, width, height }: ProviderIconType) {
   const { providers, isActive, isReady } = useWallet()
@@ -21,6 +33,6 @@ export default function ProviderIcon({ providerId, width, height }: ProviderIcon
   }
 
   return (
-    <Image unoptimized src={provider.metadata.icon} width={width ?? 50} height={height ?? 50} alt={`${provider.metadata.name} icon`} className="rounded-full shadow-md" />
+    <Image src={providerIconImages[provider.metadata.id]} width={width ?? 50} height={height ?? 50} alt={`${provider.metadata.name} icon`} className="rounded-full shadow-md" />
   )
 }
